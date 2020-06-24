d92c8b8d0fa917f2d71337a898cf69f6275391d5
Kailash Kandel <kailashkandel@gmail.com>
2020-06-24 17:25:36 +0545

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'src/widgets/loginPage.dart';
 
 void main() => runApp(TourMendApp());
 
 class TourMendApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
-      title: "TourMend",
-      // theme: ,
-      home: _MyLoginPage(),
+      title: 'TourMend',
+      theme: ThemeData(
+        primaryColor: Colors.lightBlue,
+        accentColor: Colors.greenAccent,
+        textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
+          bodyText2: GoogleFonts.montserrat(textStyle: textTheme.bodyText2),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
-  }
-}
-
-class _MyLoginPage extends StatefulWidget {
-  @override
-  _MyLoginPageState createState() => _MyLoginPageState();
-}
-
-class _MyLoginPageState extends State<_MyLoginPage>
-    with SingleTickerProviderStateMixin {
-  AnimationController _logoAnimationController;
-  Animation<double> _logoAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _logoAnimationController = new AnimationController(
-      vsync: this,
-      duration: new Duration(milliseconds: 100),
-    );
-
-    _logoAnimation = new CurvedAnimation(
-      parent: _logoAnimationController,
-      curve: Curves.easeOut,
-    );
-
-    _logoAnimation.addListener(() => this.setState(() {}));
-    _logoAnimationController.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.greenAccent,
-        body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            // Image(
-            //   image: AssetImage(''),
-            // ),
-            Column(              
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FlutterLogo(
-                  size: _logoAnimation.value * 100,
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 50),
-                  child: Text(
-                    "TOURMEND",
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontFamily: 'Google Sans',
-                      fontWeight: FontWeight.bold,
-                    ),
-                    ),
-                )
-              ],
-            ),
-
-            Container(
-              padding: EdgeInsets.only(bottom: 50),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      MaterialButton(
-                        color: Colors.white,
-                        minWidth: 100,
-                        elevation: 10,
-                        onPressed: () => {},
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontFamily: 'Google Sans',
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ),
-                      MaterialButton(
-                        color: Colors.white,
-                        minWidth: 100,
-                        elevation: 10,
-                        onPressed: () => {},
-                        child: Text(
-                          "Sign Up",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontFamily: 'Google Sans',
-                            color: Colors.black87,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
   }
 }
