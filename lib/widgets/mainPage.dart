550072c8543eb9585fcaa9aededfdb311ec07183
Kailash Kandel <kailashkandel@gmail.com>
2020-07-02 21:33:47 +0545

    Home page UI changes

--- lib/widgets/mainPage.dart
+++ lib/widgets/mainPage.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:ff_navigation_bar/ff_navigation_bar.dart';
 import 'package:flutter_app/widgets/mainPageWidgets/homePage.dart';
-import './loginPage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import './mainPageWidgets/homePage.dart';
 
@@ -10,6 +9,7 @@ class MainPage extends StatefulWidget {
   MainPage({Key key, this.title}) : super(key: key);
 
   final String title;
+
   @override
   _MainPageState createState() => _MainPageState();
 }
@@ -36,48 +36,25 @@ class _MainPageState extends State<MainPage> {
     return showDialog(
           context: context,
           builder: (context) => AlertDialog(
-            actionsPadding: EdgeInsets.all(10.0),
-            title: new Text('Are you sure?'),
-            content: new Text('Do you want to exit TourMend'),
+            actionsPadding: EdgeInsets.all(5.0),
+            buttonPadding: EdgeInsets.all(20.0),
+            title: Text('Are you sure?'),
+            content: Text('Do you want to exit TourMend'),
             actions: <Widget>[
-              new GestureDetector(
+              GestureDetector(
                 onTap: () => Navigator.of(context).pop(false),
-                child: Text("NO"),
+                child: Text(
+                  "NO",
+                  style: TextStyle(color: Colors.blueAccent),
+                ),
               ),
               SizedBox(height: 16),
-              new GestureDetector(
+              GestureDetector(
                 onTap: () => Navigator.of(context).pop(true),
-                child: Text("YES"),
-              ),
-            ],
-          ),
-        ) ??
-        false;
-  }
-
-  Future<bool> _handleLogout() async {
-    return showDialog(
-          context: context,
-          builder: (context) => new AlertDialog(
-            title: new Text('LogOut?'),
-            content: new Text('Are you sure that you want to logout?'),
-            actions: <Widget>[
-              new GestureDetector(
-                onTap: () => Navigator.of(context).pop(false),
-                child: Text("Cancel"),
-              ),
-              SizedBox(height: 16),
-              new GestureDetector(
-                onTap: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  prefs.remove('user_email');
-                  prefs.clear();
-
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => LoginPage()));
-                },
-                child: Text("Logout"),
+                child: Text(
+                  "YES",
+                  style: TextStyle(color: Colors.redAccent),
+                ),
               ),
             ],
           ),
@@ -110,50 +87,7 @@ class _MainPageState extends State<MainPage> {
     return WillPopScope(
       onWillPop: _onBackPressed,
       child: new Scaffold(
-        appBar: new AppBar(
-          title: Center(
-              child: Text("TourMend", style: TextStyle(color: Colors.white))),
-          actions: <Widget>[
-            FlatButton(
-                onPressed: () {},
-                child: Icon(Icons.notifications, color: Colors.white)),
-          ],
-        ),
         body: Center(child: _widgetOption.elementAt(_selectedIndex)),
-        drawer: Drawer(
-            child: ListView(children: <Widget>[
-          DrawerHeader(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: <Color>[Colors.deepOrange, Colors.orangeAccent])),
-              child: Container(
-                child: Column(children: <Widget>[
-                  Material(
-                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                      elevation: 10,
-                      child: Padding(
-                          padding: EdgeInsets.all(0.0),
-                          child: CircleAvatar(
-                            backgroundImage: ExactAssetImage(''),
-                            radius: 50.0,
-                            child: Image.asset(
-                              'lib/assets/leo_messi.jpg',
-                              fit: BoxFit.cover,
-                              width: 80,
-                              height: 80,
-                            ),
-                          )))
-                ]),
-              )),
-          CustomListTile(Icons.person, 'Profile', () => {}),
-          CustomListTile(Icons.notifications, 'Notification', () => {}),
-          CustomListTile(Icons.settings, 'Setting', () => {}),
-          CustomListTile(
-            Icons.lock,
-            'Log Out',
-            _handleLogout,
-          )
-        ])),
         bottomNavigationBar: FFNavigationBar(
           theme: FFNavigationBarTheme(
             barBackgroundColor: Colors.white70,
@@ -174,8 +108,8 @@ class _MainPageState extends State<MainPage> {
               label: 'Home',
             ),
             FFNavigationBarItem(
-              iconData: Icons.place,
-              label: 'Places',
+              iconData: Icons.event,
+              label: 'Events',
             ),
             FFNavigationBarItem(
               iconData: Icons.assignment,
@@ -189,43 +123,5 @@ class _MainPageState extends State<MainPage> {
         ),
       ),
     );
-  }
-}
-
-class CustomListTile extends StatelessWidget {
-  final IconData icon;
-  final String text;
-  final Function onTap;
-
-  CustomListTile(this.icon, this.text, this.onTap);
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-        child: Container(
-            decoration: BoxDecoration(
-                border:
-                    Border(bottom: BorderSide(color: Colors.grey.shade400))),
-            child: InkWell(
-                splashColor: Colors.orangeAccent,
-                onTap: onTap,
-                child: Container(
-                  height: 40,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Row(children: <Widget>[
-                          Icon(icon),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              text,
-                              style: TextStyle(fontSize: 16.0),
-                            ),
-                          ),
-                        ]),
-                        Icon(Icons.arrow_right)
-                      ]),
-                ))));
   }
 }
--- lib/widgets/mainPageWidgets/homePage.dart
+++ lib/widgets/mainPageWidgets/homePage.dart
@@ -1,5 +1,8 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../loginPage.dart';
+import './customDialogBox.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,8 +15,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  SharedPreferences preferences;
+  String userEmail;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserEmail();
+  }
+
   final CameraPosition _initialPosition =
-      CameraPosition(target: LatLng(28.260075, 83.970093));
+      CameraPosition(target: LatLng(28.260075, 83.970093), zoom: 13.0);
 
   GoogleMapController _mapController;
 
@@ -31,6 +43,7 @@ class _HomePageState extends State<HomePage> {
     var id = Random().nextInt(100);
 
     setState(() {
+      _markers.clear();
       _markers.add(
         Marker(
             markerId: MarkerId(id.toString()),
@@ -41,20 +54,111 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Future<bool> _handleLogout() async {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            actionsPadding: EdgeInsets.all(8.0),
+            buttonPadding: EdgeInsets.all(20.0),
+            title: new Text(
+              'LogOut?',
+            ),
+            content: new Text('Are you sure that you want to logout?'),
+            actions: <Widget>[
+              InkWell(
+                onTap: () => Navigator.of(context).pop(false),
+                child:
+                    Text("Cancel", style: TextStyle(color: Colors.blueAccent)),
+              ),
+              SizedBox(height: 16),
+              InkWell(
+                onTap: () async {
+                  preferences.remove('user_email');
+                  preferences.clear();
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                    ),
+                    (route) => false,
+                  );
+                },
+                child:
+                    Text("Logout", style: TextStyle(color: Colors.redAccent)),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
+
+  _getUserEmail() async {
+    preferences = await SharedPreferences.getInstance();
+    userEmail = preferences.getString('user_email');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _initialPosition,
-        mapType: MapType.normal,
-        onMapCreated: (controller) =>
-            setState(() => _mapController = controller),
-        onTap: (position) {
-          _mapController.animateCamera(CameraUpdate.newLatLng(position));
-          _addMarker(position);
-        },
-        markers: _markers.toSet(),
-      ),
+      body: Stack(children: <Widget>[
+        GoogleMap(
+          initialCameraPosition: _initialPosition,
+          mapType: MapType.normal,
+          onMapCreated: (controller) =>
+              setState(() => _mapController = controller),
+          onTap: (position) {
+            _mapController.animateCamera(CameraUpdate.newLatLng(position));
+            _addMarker(position);
+          },
+          markers: _markers.toSet(),
+        ),
+        Positioned(
+          top: 30,
+          right: 15,
+          left: 15,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.go,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                        hintText: "Search here"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: InkWell(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          child: CustomDialogBox(
+                            userEmail: userEmail,
+                            logoutFunciton: _handleLogout,
+                          ));
+                    },
+                    child: CircleAvatar(
+                        radius: 15.0,
+                        backgroundColor: Colors.deepPurple,
+                        child: Icon(
+                          Icons.portrait,
+                          size: 20.0,
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
