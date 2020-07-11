2bdcf370f3abeba197c2566cf0ea8e42803ba75a
Kailash Kandel <kailashkandel@gmail.com>
2020-07-12 00:18:26 +0545

    Added updated php files

--- php/placesJson.php
+++ php/placesJson.php
@@ -1,27 +1,18 @@
 <?php   
-           include('db_config.php');
-           $sql = "SELECT * FROM tbl_places";  
-           
-// Creating SQL command to fetch all records from Fruits Table.
-
-  
-$result = $db_conn->query($sql);
+    include('db_config.php');
+    $sql = "SELECT * FROM tbl_places";  
+            
+    $result = $db_conn->query($sql);
  
-if ($result->num_rows >0) {
+    if ($result->num_rows >0) {
+          while($row[] = $result->fetch_assoc()) {
+             $item = $row;
+             $json = json_encode($item);
  
- 
- while($row[] = $result->fetch_assoc()) {
- 
- $item = $row;
- 
- $json = json_encode($item);
- 
- }
- 
-} else {
- echo "No Data Found.";
-}
- echo $json;
- $db_conn->close();
- 
+            }
+    } else {
+        echo "No Data Found.";
+    }
+     echo $json;
+    $db_conn->close();
 ?>
