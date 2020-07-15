7094ed5371ba50d3f7702e2c74b9310c52265a2d
Kailash Kandel <kailashkandel@gmail.com>
2020-07-15 22:52:41 +0545

    Added placesJson.php file

--- php/placesJson.php
+++ php/placesJson.php
@@ -1,18 +1,44 @@
-<?php   
-    include('db_config.php');
-    $sql = "SELECT * FROM tbl_places";  
-            
-    $result = $db_conn->query($sql);
- 
-    if ($result->num_rows >0) {
-          while($row[] = $result->fetch_assoc()) {
-             $item = $row;
-             $json = json_encode($item);
- 
+<?php
+
+if (isset($_GET['page_number'])) {
+
+    require_once 'db_config.php';
+
+    $page_number = $_GET['page_number'];
+    $item_count = 3;
+    // array for the final response
+    $response = array();
+
+    $to = ($page_number - 1) * $item_count;
+
+    $sql = "SELECT * FROM tbl_places ORDER BY placename LIMIT $item_count OFFSET $to";
+
+    $result = mysqli_query($db_conn, $sql);
+
+    if ($result) {
+        if (mysqli_num_rows($result) <= 4 && mysqli_num_rows($result) != 0) {
+            $response['statusCode'] = '1';
+            $response['message'] = 'Data fetched successfully!';
+            $rowArray =  array();
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $rowItems = array('id' => $row['id'], 'placeName' => $row['placename'], 'imgURL' => $row['placeimage'], 'destination' => $row['dst']);
+                array_push($rowArray, $rowItems);
             }
+
+            $response['places'] = $rowArray;
+            echo json_encode($response);
+        } else {
+            $response['statusCode'] = '2';
+            $response['message'] = 'No more data available!';
+
+            echo json_encode($response);
+        }
+        mysqli_close($db_conn);
     } else {
-        echo "No Data Found.";
+        $response['statusCode'] = '0';
+        $response['message'] = 'Couldn\'t fetch data! Internal server error.';
+        echo json_encode($response);
     }
-     echo $json;
-    $db_conn->close();
-?>
+} else echo json_encode(array('statusCode' => '3', 'message' => 'Error in method!'));
+return;
